60d821631f92572d816886e1b3e614a29e837fc2
Caden Maxwell <cadenr.maxwell@gmail.com>
2024-03-07 20:14:57 -0700

    Add project files.

---
+++ Lander/LunarLanderGame.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace Lander;
+
+public class LunarLanderGame : Game
+{
+    private GraphicsDeviceManager m_graphics;
+    private SpriteBatch m_spriteBatch;
+
+    public LunarLanderGame()
+    {
+        m_graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        MyComponent myComponent = new MyComponent(this);
+        this.Components.Add(myComponent);
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        m_spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        // TODO: use this.Content to load your game content here
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        // TODO: Add your update logic here
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        // TODO: Add your drawing code here
+
+        base.Draw(gameTime);
+    }
+}
---
+++ Lander/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new Lander.LunarLanderGame();
+game.Run();
