327ced31c450c12783fa2f8438a4bfc1dc4f06f2
cmax396 <109488523+caden-maxwell@users.noreply.github.com>
2024-03-07 20:14:57 -0700

    Add project files.

---
+++ Lander/MyComponent.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.Xna.Framework;
+using System;
+using System.Diagnostics;
+
+namespace Lander;
+
+class MyComponent : GameComponent
+{
+    public MyComponent(Game game) : base(game) { }
+
+    public override void Update(GameTime gameTime)
+    {
+        Debug.WriteLine("GameComponent Updated", gameTime);
+
+        base.Update(gameTime);
+    }
+}
