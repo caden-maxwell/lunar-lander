810d3982f70c9b19275bb3ab8079fcb563f3c933
cmax <cadenr.maxwell@gmail.com>
2024-03-17 14:29:18 -0600

    uncap framerate

--- LunarLander/GameState/GamePlayView.cs
+++ LunarLander/GameState/GamePlayView.cs
@@ -3,10 +3,8 @@ using LunarLander.Input;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 
 namespace LunarLander;
 
